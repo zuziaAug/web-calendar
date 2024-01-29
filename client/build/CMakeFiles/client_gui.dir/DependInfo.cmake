bcc86483601f84c3a0d9d77b49da8e424d75a06f
Zuzanna Augustyniak <116178665+zuziaAug@users.noreply.github.com>
2024-01-29 12:24:57 +0100

    Add files via upload

---
+++ client/build/CMakeFiles/client_gui.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/oko/web-calendar/client/build/client_gui_autogen/mocs_compilation.cpp" "CMakeFiles/client_gui.dir/client_gui_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/client_gui.dir/client_gui_autogen/mocs_compilation.cpp.o.d"
+  "/home/oko/web-calendar/client/main.cpp" "CMakeFiles/client_gui.dir/main.cpp.o" "gcc" "CMakeFiles/client_gui.dir/main.cpp.o.d"
+  "/home/oko/web-calendar/client/mainwindow.cpp" "CMakeFiles/client_gui.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/client_gui.dir/mainwindow.cpp.o.d"
+  "/home/oko/web-calendar/client/receiverthread.cpp" "CMakeFiles/client_gui.dir/receiverthread.cpp.o" "gcc" "CMakeFiles/client_gui.dir/receiverthread.cpp.o.d"
+  "/home/oko/web-calendar/client/senderthread.cpp" "CMakeFiles/client_gui.dir/senderthread.cpp.o" "gcc" "CMakeFiles/client_gui.dir/senderthread.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ client/build/CMakeFiles/client_gui.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_gui_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/client_gui_autogen.dir/ParseCache.txt"
+  "client_gui_autogen"
+  "CMakeFiles/client_gui.dir/client_gui_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/client_gui.dir/client_gui_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/client_gui.dir/main.cpp.o"
+  "CMakeFiles/client_gui.dir/main.cpp.o.d"
+  "CMakeFiles/client_gui.dir/mainwindow.cpp.o"
+  "CMakeFiles/client_gui.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/client_gui.dir/receiverthread.cpp.o"
+  "CMakeFiles/client_gui.dir/receiverthread.cpp.o.d"
+  "CMakeFiles/client_gui.dir/senderthread.cpp.o"
+  "CMakeFiles/client_gui.dir/senderthread.cpp.o.d"
+  "client_gui"
+  "client_gui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
