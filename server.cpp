e6ebd80e5307637b09ce671bb926fbbec90d21c7
Zuzanna Augustyniak <116178665+zuziaAug@users.noreply.github.com>
2024-01-31 09:20:08 +0100

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -68,8 +68,10 @@ Request parseRequest(const char* buffer) {
     istringstream iss(buffer);
     Request request;
     iss >> request.client_id >> request.action >> ws; 
-    getline(iss, request.event_title, ' ');
-    getline(iss, request.event_description, ' ');
+    if (request.action == "ADD_EVENT") {
+        getline(iss, request.event_title, ' ');
+        getline(iss, request.event_description, ' ');
+    }
     iss >> request.start_date.year >> request.start_date.month >> request.start_date.day >> request.start_date.hour
         >> request.end_time.year >> request.end_time.month >> request.end_time.day >> request.end_time.hour
         >> request.event_id;
